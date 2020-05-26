ef3a01f5e6ce327087e2c3dff3c52e89ee5743c2
leesumin0961 <tnals0961@hanmail.net>
2020-05-26 18:11:21 +0900

    COMMIT

---
+++ DateType/src/DateType.java
@@ -0,0 +1,13 @@
+public class DateType {
+
+	public static void main(String[] args) {
+		 
+		//자바에서는 다음과 같이 문자열과 기본 타입의 +연산이 실행되면 기본 타입의 값이 문자열로 바뀌고 두 문자열이 연결된 새로운 문자열이 생성된다.
+		
+		String toolName = "JDK";
+		//toolName +1.8;
+		System.out.println(toolName + "이 출시됨");
+	 
+	}
+
+}
