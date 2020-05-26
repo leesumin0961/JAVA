29cc4ff006881f60a84985fdb403970fd5bb511f
leesumin0961 <tnals0961@hanmail.net>
2020-05-26 18:16:02 +0900

    COMMIT

---
+++ TernaryOperator/src/TernaryOperator.java
@@ -0,0 +1,20 @@
+
+
+public class TernaryOperator {
+
+	public static void main(String[] args) {
+		
+		//조건연산자
+			
+		int x = 5;
+		int y = 3;
+		System.out.println((x>y)?1:-1);	//x가 y보다 크기 때문에 1이 된다.
+		
+		
+		int a = 3 , b = 5;
+		
+		System.out.println("두 수의 차는  " + ((a>b)?(a-b):(b-a)));
+				
+	}
+
+}
