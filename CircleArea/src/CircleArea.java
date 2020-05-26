88f27374f0c6c9ff537b8c2cbd97fa514fca96fa
leesumin0961 <tnals0961@hanmail.net>
2020-05-26 18:09:39 +0900

    FIRSTCOMMIT

---
+++ CircleArea/src/CircleArea.java
@@ -0,0 +1,18 @@
+
+public class CircleArea {
+
+	public static void main(String[] args) {
+		//상수 PI를 선언하고 원의 면적을 구하는 프로그램
+		 
+				final double PI = 3.14;	//원주율을 상수로 선언
+				 
+				double radius = 10.0; //원의 반지름
+				double circleArea = radius * radius * PI;
+				
+				//원의 면적을 화면에 출력한다.
+				
+				System.out.println("원의 면적 " + circleArea);
+
+	}
+
+}
