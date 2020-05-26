458edd73de57cf47d74ee69d7f6e9619e796a786
leesumin0961 <tnals0961@hanmail.net>
2020-05-26 18:16:51 +0900

    COMMIT

---
+++ variable/src/variable.java
@@ -0,0 +1,25 @@
+
+public class variable {
+
+	public static void main(String[] args) {
+
+		//변수선언
+		
+		int price;
+		
+		//변수선언과 동시에 초기화
+		
+		int radius = 10;
+	    String c1 = "a", c2 = "b", c3 = "c";	
+		double weight = 75.56;
+		
+		//변수 읽기와 저장
+		
+		radius = 10 * 5;
+		c1 = "r";
+		weight = weight + 5.0;
+		
+
+	}
+
+}
