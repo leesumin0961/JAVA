4b6c718b7f4f261c4227a8dc49c07c81a39b7cf5
leesumin0961 <tnals0961@hanmail.net>
2020-05-26 18:13:30 +0900

    COMMIT

---
+++ Hello/src/Hello.java
@@ -0,0 +1,20 @@
+
+
+public class Hello {
+	
+	public static int sum(int n, int m) {
+		return n + m;
+	}
+	 
+	public static void main(String[] args) {
+		int i = 20;
+		int s;
+		char a;
+		
+		s = sum(i,10);	//메소드 호출
+		a = '?';	
+		System.out.println(a);	// 문자 '?' 호출
+		System.out.println("Hello");	//"Hello" 문자열 출력
+		System.out.println(s);	//정수 s 값 30 출력
+	}
+}
